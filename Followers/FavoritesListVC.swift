22874710429cd1a88ef0e8e7999fa2a5a596c6b0
SH <han@SHs-iMac.fios-router.home>
2022-03-07 21:25:26 -0500

    first commit

---
+++ Followers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesVC.swift
+//  Followers
+//
+//  Created by SH on 3/7/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
